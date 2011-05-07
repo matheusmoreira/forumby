12ab3323266dfb42ae0cdeef2a73d01aadb371b7
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-05-07 11:11:44 -0300

    Including jQuery in the Application.

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,10 @@ module Forumby
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(rails)
+
+    # :jquery expansion for jQuery JavaScript files.
+    config.action_view.javascript_expansions[:jquery] = %w(jquery.min jquery-ui.min)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
