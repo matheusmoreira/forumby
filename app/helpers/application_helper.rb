9e0c53f117d6a2bb1040837eca0e262ea5f5d771
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-06-03 23:41:28 -0300

    Added links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   include FormHelper
   include ListHelper
 
+  def link_button_to(body, url, html_options = {})
+    html_options.merge!(:class => 'button') { |key, old, new| "#{old} #{new}" }
+    link_to body, url, html_options
+  end
+
 end
