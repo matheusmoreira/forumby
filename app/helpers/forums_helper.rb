22efcee700527784afff82cd2aad53e72fd83db0
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-06-12 18:14:51 -0300

    Updated forum style.

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,5 +1,12 @@
 module ForumsHelper
 
+  def forum_name_for(forum)
+    link_with_title_to forum.name,
+                       forum_path(forum),
+                       forum.description,
+                       :class => 'name'
+  end
+
   def nested_forums_for(forum, partial = 'forums/nested_forums/list')
     render :partial => partial, :collection => forum.nested_forums
   end
